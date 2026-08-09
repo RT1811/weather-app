71256d32d7cb41c5ebb4d1e473ee6af6917ee123
RT1811 <ritwick.thebest@gmail.com>
2026-08-08 19:58:24 -0700

    Basic fetch for weather data and templates

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 // src/index.js
 import "./styles.css";
+import { API_KEY } from "./config.js";
 
-console.log("It's working!");
+async function getWeatherData(location) {
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodeURIComponent(location)}?unitGroup=metric&key=${API_KEY}&contentType=json`;
+
+    const response  = await fetch(url);
+
+    if (!response.ok) {
+        throw new Error(`Weather Request Failed: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+}
+
+getWeatherData("London").then((data) => console.log(data.currentConditions));
